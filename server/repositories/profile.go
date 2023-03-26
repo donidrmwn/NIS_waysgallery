dc7f6f7eb0dc47fb76876708c74228db76a0fc1c
donidarmawan <44544869+zuro64@users.noreply.github.com>
2023-03-27 06:13:07 +0700

    test

--- server/repositories/profile.go
+++ server/repositories/profile.go
@@ -36,6 +36,6 @@ func (r *repository) UpdateProfile(profile models.Profile) (models.Profile, erro
 func (r *repository) SearchProfile(profileName string) ([]models.Profile, error) {
 	var profiles []models.Profile
 	searchProfileName := "%" + profileName
-	err := r.db.Where("name ilike ?", searchProfileName, searchProfileName).Preload("User").Find(&profiles).Error
+	err := r.db.Where("name ilike ?", searchProfileName).Preload("User").Find(&profiles).Error
 	return profiles, err
 }
