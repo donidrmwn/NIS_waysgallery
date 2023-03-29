b37ef0897453364aa5d3a6973ed8fdfa3cda34c8
donidarmawan <44544869+zuro64@users.noreply.github.com>
2023-03-29 10:59:06 +0700

    testtest

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,6 @@ function App() {
   const checkUser = async () => {
     try {
       const response = await API.get('/check-auth');
-      console.log("check user success : ", response)
       // Get user data
       let payload = response.data.data;
       // Get token from local storage
