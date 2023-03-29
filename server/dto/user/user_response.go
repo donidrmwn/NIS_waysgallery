083a118633bd016c1506adf6a2e0b190a1f6c8d3
donidarmawan <44544869+zuro64@users.noreply.github.com>
2023-03-29 10:11:04 +0700

    test

--- server/dto/user/user_response.go
+++ server/dto/user/user_response.go
@@ -3,6 +3,7 @@ package userdto
 import profiledto "waysgallery/dto/profile"
 
 type UserPostResponse struct {
+	ID      int                            `json:"id"`
 	Email   string                         `json:"email"`
 	Profile profiledto.ProfileResponsePost `json:"profile"`
 }
