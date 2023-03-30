6fe66092199100604413a0abdf2d33c7a5da87cb
donidarmawan <44544869+zuro64@users.noreply.github.com>
2023-03-30 08:44:00 +0700

    test

--- server/handlers/auth.go
+++ server/handlers/auth.go
@@ -90,7 +90,8 @@ func (h *handlerAuth) Register(c echo.Context) error {
 }
 
 func (h *handlerAuth) Login(c echo.Context) error {
-	request := new(authdto.LoginRequest)
+	// request := new(authdto.LoginRequest)
+	var request *authdto.LoginRequest
 	if err := c.Bind(request); err != nil {
 		return c.JSON(http.StatusBadRequest, dto.ErrorResult{
 			Code:    http.StatusBadRequest,
