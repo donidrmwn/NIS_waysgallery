9e21e56ce2701213ffbc5fb1494b3b39b574fd67
donidarmawan <44544869+zuro64@users.noreply.github.com>
2023-04-03 07:50:57 +0700

    test

--- server/main.go
+++ server/main.go
@@ -25,7 +25,7 @@ func main() {
 	database.RunMigration()
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"https://nis-waysgallery.vercel.app"},
+		AllowOrigins: []string{"*"},
 		AllowMethods: []string{echo.GET, echo.PATCH, echo.DELETE, echo.POST},
 		AllowHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"},
 	}))
