55327b5842e642e019b14a069a8f75cc4b566fb3
donidarmawan <44544869+zuro64@users.noreply.github.com>
2023-04-03 07:23:09 +0700

    test

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -8,34 +8,62 @@ import ModalRegister from '../components/Modals/ModalRegister'
 
 export default function LandingPage() {
 
+    // const style = {
+    //     vector1: {
+    //         position: "absolute",
+    //         width: "636px",
+    //         height: "628px",
+    //         left: "-90px",
+    //         top: "-40px",
+
+    //     },
+    //     vector2: {
+    //         position: "absolute",
+    //         width: "468px",
+    //         height: "428px",
+    //         left: "0",
+    //         bottom: "0",
+
+    //     }
+    //     ,
+    //     vector3: {
+    //         position: "absolute",
+    //         width: "468px",
+    //         height: "428px",
+    //         right: "0",
+    //         bottom: "0",
+
+    //     }
+    // }
+
     const style = {
         vector1: {
             position: "absolute",
-            width: "636px",
-            height: "628px",
-            left: "-90px",
-            top: "-40px",
+            width: "25em",
+            height: "25em",
+            left:"-3em",
+            top:"-3em"
+            
 
         },
         vector2: {
             position: "absolute",
-            width: "468px",
-            height: "428px",
-            left: "0",
-            bottom: "0",
+            width: "25em",
+            height: "25em",
+            left:"-3em",
+            bottom: "-3em",
 
         }
         ,
         vector3: {
             position: "absolute",
-            width: "468px",
-            height: "428px",
-            right: "0",
-            bottom: "0",
+            width: "25em",
+            height: "25em",
+            right: "-3em",
+            bottom: "-3em",
 
         }
     }
-
     const [showRegister, setShowRegister] = useState(false)
     const handleShowRegister = () => setShowRegister(true)
     const handleCloseRegister = () => setShowRegister(false)
@@ -53,18 +81,18 @@ export default function LandingPage() {
     }
     return (
         <>
-            <Container fluid="true">
+            <Container fluid="true" className='position-relative '>
                 <Image src='/Vector 1.png' style={style.vector1} />
                 <Image src='/Vector 2.png' style={style.vector2} />
                 <Image src='/Vector 3.png' style={style.vector3} />
-                <Row fluid="true" className='w-100 d-flex p-5 align-items-center p-5' style={{ zIndex: -1 }}>
-                    <Col md="5" className='d-grid justify-content-center align-items-center ps-5'>
-                        <Row>
+                <Row fluid="true" className='w-100 vh-100 d-flex  align-items-center mt-5' style={{ zIndex: -1 }}>
+                    <Col sm="12" md="12" lg="6" className='d-grid justify-content-center align-items-center '>
+                        <Row className='position-relative'>
                             <BrandLanding fontSize={"100px"} />
                             <BrandImage
-                                top={"130px"}
-                                width={"300px"}
-                                left={"330px"}
+                                top={"-5em"}
+                                width={"17em"}
+                                left={"12em"}
                             />
                         </Row>
                         <Row className='mt-3'>
@@ -78,7 +106,7 @@ export default function LandingPage() {
                         </Col>
 
                     </Col>
-                    <Col md="6" className='d-flex justify-content-center align-items-center'>
+                    <Col sm="12" md="12" lg="6" className='d-flex justify-content-center align-items-center mt-4'>
                         <Image className='w-100' src='/g10.png' />
                     </Col>
                 </Row>
